49b7396a616701e803b1b5b22c24c0967b3fd68b
SamuelMedina07 <semedina@uth.hn>
2025-06-30 23:27:51 -0600

    En App.js hice una prueba sin libreria para ver las rutas del directorio pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import './pages/Home'
+import Home from './pages/Home';
+import './pages/Sobre'
+import Sobre from './pages/Sobre';
 
 function App() {
+
+  const location = window.location.pathname
+  console.log(location)
+
+  const Router = () => location === "/" ? <Home /> : <Sobre />;
+
+/* const Router = () => {
+    if(location === "/"){
+      return <Home/>}
+    else {
+      return <Sobre/>}
+  } */
   return (
     <>
-
+<Router/>
     </>
   );
 }
