b2fd58938c141eb6593379bda7a277a09bcd73fe
SamuelMedina07 <semedina@uth.hn>
2025-07-03 15:52:02 -0600

    Agregado el componente Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
-import './pages/Home'
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
 import Error404 from './pages/Error404'
+import Header from './components/Header';
 
 import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
@@ -12,6 +12,7 @@ function App() {
   return (
     <>
 <Router>
+  <Header/>
 <Routes>
   <Route path='/' element={<Home/>} />
   <Route path='/sobre' element={<Sobre/>} />
